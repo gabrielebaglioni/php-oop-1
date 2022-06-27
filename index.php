0792d542134cb052b1185b7a3709386aaada81d8
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-27 15:55:51 +0200

    metodo migliorato + poco ma veramanre poco style implementato

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
    
    $movies = [
       new movie('topgun', 'Joseph Kosinski', 'action', '1986', 110),
-      new movie('hustle', 'Jeremiah Zagar', 'comedy/sport', '2022', 19)
+      new movie('hustle', 'Jeremiah Zagar', 'comedy/sport', '2022', 117),
+      new movie('the office', 'Steve Carell', 'comedy', '2013', 15 )
    ];
    
  ?>
@@ -20,16 +21,17 @@
     <ul>
         <?php foreach($movies as $movie) { ?>
         <li>
-            <h3><?php echo $movie->title; ?></h3>
-            <h4><?php echo $movie->movieDirector; ?></h4>
-            <h4><?php echo $movie->genre; ?></h4>
-            <h4><?php echo $movie->year; ?></h4>
+            <h3>title: <?php echo $movie->title; ?></h3>
+            <h4>movieDirector: <?php echo $movie->movieDirector; ?></h4>
+            <h4>gener: <?php echo $movie->genre; ?></h4>
+            <h4>original release: <?php echo $movie->year; ?></h4>
             <?php if($movie->runningTime < 20 ) { ?>
             <strong> non è un film, è una serie tv </strong>
             <?php } ?>
             <?php  { ?>
             <strong>durata: <?php echo $movie->runningTime; ?>M</strong>
             <?php } ?>
+            <hr style="width: 100%; color: black;">
         </li>
         <?php } ?>
     </ul>
