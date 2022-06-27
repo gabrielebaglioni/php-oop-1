66844c2f33c5308e557ab52ded3f927fb17ecc66
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-27 15:09:04 +0200

    construct impostato

--- movie.php
+++ movie.php
@@ -1,3 +1,21 @@
 <?php
+  class movie {
+   public $title;
+   public $movieDirector;
+   public $genre;
+   public $year;
+   public $runningTime;
 
+
+   public function __construct($_title, $_movieDirector, $_genre, $_year, $_runningTime){
+    
+      $this->title = $_title;
+      $this->movieDirector = $_movieDirector;
+      $this->genre = $_genre;
+      $this->year = $_year;
+      $this->runningTime = $_runningTime;
+    }
+  }
+
+ 
 ?>
